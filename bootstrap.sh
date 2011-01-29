be3cea6f58bf022f83f8d833b595b98db9facec3
Lars Smit <lars@larssmit.com>
2011-01-29 08:21:09 +0100

    Removed testcode

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-#git clone git://github.com/carlhuda/janus.git ~/.vim
-git clone git://github.com/larssmit/janus.git ~/.vim
+git clone git://github.com/carlhuda/janus.git ~/.vim
 cd ~/.vim
 rake
