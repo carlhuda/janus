ecb97b1b8dd56140e71db743487f91a9402e0126
Curtis Schofield <github@robotarmyma.de>
2011-02-25 10:42:06 -0800

    Edited bootstrap.sh via GitHub

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-git clone git://github.com/carlhuda/janus.git ~/.vim
+git clone git://github.com/robotarmy/jan.git ~/.vim
 cd ~/.vim
 rake
