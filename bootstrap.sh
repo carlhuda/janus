4b0ef3e26f9cf2034c11ff2e00723f5d57060e48
Robb Kidd <robb.kidd@gd-ais.com>
2011-05-16 15:14:34 -0400

    Added GIT_HTTPS_ONLY env var to set git to use https instead of ssh. #refactotum

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,14 @@
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-git clone git://github.com/carlhuda/janus.git ~/.vim
+echo "*  Attempting to clone with SSH."
+if ! git clone git://github.com/carlhuda/janus.git ~/.vim ; then
+  echo "*  SSH doesn't work; are you behind a firewall? Let's try HTTPS."
+  if git clone https://github.com/carlhuda/janus.git ~/.vim ; then
+    echo "*  HTTPS works. We'll use that to install the rest."
+    export GIT_HTTPS_ONLY=true
+  else
+    fail "*  Unable to clone the janus repository, attempted both git:// and https://"
+  fi
+fi
 cd ~/.vim
 rake
+unset GIT_HTTPS_ONLY
