2f20aef2d20a6093b57ef841defb0ea1e4a4833f
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-10 14:13:43 +0100

    Bootstrap: Source rbenv as well

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,13 @@ if ! type rvm &> /dev/null; then
   [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 fi
 
+# Source Rbenv so vimius .rbenv-version takes effect
+if ! type rbenv &> /dev/null; then
+  if [[ -s $HOME/.rvm/scripts/rvm ]]; then
+    eval "`$HOME/.rbenv/bin/rbenv init -`"
+  fi
+fi
+
 # Add <strong>.old</strong> to any existing Vim file in the home directory
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do
   if [[ ( -e $i ) || ( -h $i ) ]]; then
