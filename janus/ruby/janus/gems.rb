a7109ed04d11e3aa8830d9eb4907dd07c1c4fa5f
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-20 17:45:47 +0100

    Gems: find_all_by_name was added to Gem::Specification in Rubygems 1.8
    closes #294

--- janus/ruby/janus/gems.rb
+++ janus/ruby/janus/gems.rb
@@ -7,7 +7,7 @@ module Janus
   def find_gem(gem_name, *requirements)
     begin
       require 'rubygems'
-      if Gem.const_defined?(:Specification)
+      if Gem.const_defined?(:Specification) && Gem::Specification.respond_to?(:find_all_by_name)
         Gem::Specification.find_all_by_name(gem_name, *requirements)
       elsif Gem.respond_to?(:source_index)
         Gem.source_index.find_name(gem_name, *requirements)
