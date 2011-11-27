3b864893c5a697968092bfdff26cc43767e35f5a
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-27 19:49:19 +0100

    Bootstrap: Add error handling
    closes #299

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,16 +9,24 @@ if ! type rbenv &> /dev/null; then
   fi
 fi
 
+function die()
+{
+    echo "${@}"
+    exit 1
+}
+
 # Add <strong>.old</strong> to any existing Vim file in the home directory
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do
   if [[ ( -e $i ) || ( -h $i ) ]]; then
     echo "${i} has been renamed to ${i}.old"
-    mv "${i}" "${i}.old"
+    mv "${i}" "${i}.old" || die "Could not move ${i} to ${i}.old"
   fi
 done
 
 # Clone Janus into .vim
-git clone -b experimental git://github.com/carlhuda/janus.git $HOME/.vim
+git clone -b experimental git://github.com/carlhuda/janus.git $HOME/.vim \
+  || die "Could not clone the repository to ${HOME}/.vim"
 
 # Run rake inside .vim
-( cd $HOME/.vim && rake )
+cd $HOME/.vim || die "Could not go into the ${HOME}/.vim"
+rake || die "Rake failed."
