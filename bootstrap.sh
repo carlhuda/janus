9300a038f0cf0f24c13779a2ea68f06c2b0242d7
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-12-27 22:37:57 +0100

    Lets stick with https

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,7 +24,7 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do
 done
 
 # Clone Janus into .vim
-git clone git://github.com/carlhuda/janus.git $HOME/.vim \
+git clone https://github.com/carlhuda/janus.git $HOME/.vim \
   || die "Could not clone the repository to ${HOME}/.vim"
 
 # Run rake inside .vim
