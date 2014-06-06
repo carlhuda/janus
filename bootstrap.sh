f615461dd80cf20fe1a4392670688d12ce5b108d
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2014-06-06 11:52:14 -0700

    bootstrap.sh: quote the paths
    closes #579

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 function die()
 {
     echo "${@}"
     exit 1
 }
 
-# Add <strong>.old</strong> to any existing Vim file in the home directory
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do
+# Add .old to any existing Vim file in the home directory
+for i in "${HOME}/.vim" "${HOME}/.vimrc" "${HOME}/.gvimrc"; do
   if [ -e $i ]; then
     echo "${i} has been renamed to ${i}.old"
     mv "${i}" "${i}.old" || die "Could not move ${i} to ${i}.old"
@@ -13,9 +15,9 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do
 done
 
 # Clone Janus into .vim
-git clone https://github.com/carlhuda/janus.git $HOME/.vim \
+git clone https://github.com/carlhuda/janus.git "${HOME}/.vim" \
   || die "Could not clone the repository to ${HOME}/.vim"
 
 # Run rake inside .vim
-cd $HOME/.vim || die "Could not go into the ${HOME}/.vim"
+cd "${HOME}/.vim" || die "Could not go into the ${HOME}/.vim"
 rake || die "Rake failed."
