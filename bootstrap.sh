a49f9eb836f4e0f19883dc1e30f68ca0bb89403c
Anderson Mesquita <andersonvom@gmail.com>
2016-12-06 14:41:47 -0600

    Account for existing installation during bootstrap
    If janus is already installed, the bootstrap script will just try to
    run rake again and update the existing installation.
    
    Alternatively, a  flag can be passed to the bootstrap script
    so that it will run the installation from scratch.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,18 +6,39 @@ function die()
     exit 1
 }
 
-# Add .old to any existing Vim file in the home directory
-for filepath in "${HOME}/.vim" "${HOME}/.vimrc" "${HOME}/.gvimrc"; do
-  if [ -e $filepath ]; then
-    mv "${filepath}" "${filepath}.old" || die "Could not move ${filepath} to ${filepath}.old"
-    echo "${filepath} has been renamed to ${filepath}.old"
+function backup_previous_install()
+{
+  # Add .old to any existing Vim file in the home directory
+  for filepath in "${HOME}/.vim" "${HOME}/.vimrc" "${HOME}/.gvimrc"; do
+    if [ -e $filepath ]; then
+      mv "${filepath}" "${filepath}.old" || die "Could not move ${filepath} to ${filepath}.old"
+      echo "${filepath} has been renamed to ${filepath}.old"
+    fi
+  done
+}
+
+function clone_janus()
+{
+  # Clone Janus into .vim
+  git clone --recursive https://github.com/carlhuda/janus.git "${HOME}/.vim" \
+    || die "Could not clone the repository to ${HOME}/.vim"
+}
+
+function run_rake()
+{
+  # Run rake inside .vim
+  pushd "${HOME}/.vim" || die "Could not go into the ${HOME}/.vim"
+  rake || die "Rake failed."
+  popd
+}
+
+function main()
+{
+  if [ ! -e "${HOME}/.vim/janus" -o "$1" == "--force" ]; then
+    backup_previous_install
+    clone_janus
   fi
-done
+  run_rake
+}
 
-# Clone Janus into .vim
-git clone --recursive https://github.com/carlhuda/janus.git "${HOME}/.vim" \
-  || die "Could not clone the repository to ${HOME}/.vim"
-
-# Run rake inside .vim
-cd "${HOME}/.vim" || die "Could not go into the ${HOME}/.vim"
-rake || die "Rake failed."
+main $1
